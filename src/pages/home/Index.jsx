05364e88068bf35b56ad304a7d50274f07fca57f
MiyaKCheng <miya1736@gmail.com>
2023-12-17 20:57:58 +0900

    modified:   src/pages/home/Index.jsx 	new file:   src/pages/steps/Steps.css 	renamed:    src/pages/home/contents/Steps.jsx -> src/pages/steps/Steps.jsx

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -9,6 +9,7 @@ import Sub from "./sub/sub.jsx";
 import Content1 from "./contents/Content1.jsx";
 import Content2 from "./contents/Content2.jsx";
 import Content3 from "./contents/Content3.jsx";
+import Steps from "../steps/Steps.jsx";
 import Ins from "./ig/Ins.jsx";
 import Info from "./info/Info.jsx";
 import Footer from "./footer/Footer.jsx";
@@ -20,7 +21,7 @@ function Index() {
     <>
       <Top />
       <div className="relative">
-
+        ß
         <Kv />
         <Sub />
         <KvBg className="absolute top-0 inline-grid" />
@@ -29,6 +30,7 @@ function Index() {
       <Content1 />
       <Content2 />
       <Content3 />
+      <Steps />
       <Ins />
       <Info />
       <Footer />
