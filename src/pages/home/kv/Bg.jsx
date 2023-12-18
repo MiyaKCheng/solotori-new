dd94352638ceadb6b61984a0d5004a7fc23be731
MiyaKCheng <miya1736@gmail.com>
2023-12-18 23:40:53 +0900

    cloud updated

--- src/pages/home/kv/Bg.jsx
+++ src/pages/home/kv/Bg.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import { useSpring, animated } from "react-spring";
 import cloud01 from "../../../assets/img/cloud01.png";
 import cloud02 from "../../../assets/img/cloud02.png";
-import cloudBg from "../../../assets/img/cloud-bg.png";
+import cloud06 from "../../../assets/img/cloud06.png";
+import cloud07 from "../../../assets/img/cloud07.png";
+import cloud08 from "../../../assets/img/cloud08.png";
+import cloudBg from "../../../assets/img/cloudBg.png";
 import kv3dCloud from "../../../assets/img/kv-3d-cloud.png";
 
 const KvBg = () => {
@@ -23,20 +26,22 @@ const KvBg = () => {
     }, []);
 
     return (
-        <div className="kv-bg-container" style={{}}>
+        <div>
             {/* Cloud Background */}
             <div
                 style={{
                     backgroundImage: `url(${cloudBg})`,
                     backgroundSize: "cover",
-                    height: "100vh",
+                    height: "900px",
+                    width: "100%",
                     position: "absolute",
-                    top: 0,
+                    top: -50,
                     left: 0,
                     right: 0,
                     bottom: 0,
                     zIndex: -50,
-                    overflow: "visitable",
+                    opacity: "0.7",
+
                 }}
             />
 
@@ -51,6 +56,7 @@ const KvBg = () => {
                     position: "absolute",
                     transform: offset.to((o) => `translateY(-${o * 0.3}px)`),
                     zIndex: -1,
+                    opacity: "0.5",
                 }}
                 alt="Cloud 01"
             />
@@ -98,7 +104,78 @@ const KvBg = () => {
                 }}
                 alt="3D Cloud"
             />
+            <animated.img
+                src={cloud01}
+                style={{
+                    display: "block",
+                    width: "100%",
+                    marginLeft: "50%",
+                    marginTop: "80%",
+                    position: "absolute",
+                    transform: offset.to((o) => `translateY(-${o * 0.8}px)`),
+                    zIndex: "-40",
+                    opacity: "0.5",
+                }}
+                alt="Cloud 01"
+            />
+            <animated.img
+                src={cloud07}
+                style={{
+                    display: "block",
+                    width: "100%",
+                    marginLeft: "-10%",
+                    marginTop: "920px",
+                    position: "absolute",
+                    transform: offset.to((o) => `translateY(-${o * 0.3}px)`),
+                    zIndex: 0,
+                    opacity: "0.6",
+                }}
+                alt="Cloud 01"
+            />
+            <animated.img
+                src={cloud06}
+                style={{
+                    display: "block",
+                    width: "100%",
+                    marginLeft: "50%",
+                    marginTop: "1100px",
+                    position: "absolute",
+                    transform: offset.to((o) => `translateY(-${o * 0.3}px)`),
+                    zIndex: 0,
+                    opacity: "0.5",
+                }}
+                alt="Cloud 01"
+            />
+            <animated.img
+                src={cloud02}
+                style={{
+                    display: "block",
+                    width: "100%",
+                    marginLeft: "-5%",
+                    marginTop: "1570px",
+                    position: "absolute",
+                    transform: offset.to((o) => `translateY(-${o * 0.3}px)`),
+                    zIndex: 0,
+                    opacity: "0.3",
+                }}
+                alt="Cloud 01"
+            />
+            <animated.img
+                src={cloud08}
+                style={{
+                    display: "block",
+                    width: "100%",
+                    marginLeft: "10%",
+                    marginTop: "1450px",
+                    position: "absolute",
+                    transform: offset.to((o) => `translateY(-${o * 0.3}px)`),
+                    zIndex: 0,
+                    opacity: "0.5",
+                }}
+                alt="Cloud 01"
+            />
         </div>
+
     );
 };
 
--- src/pages/home/sub/Sub.jsx
+++ src/pages/home/sub/Sub.jsx
@@ -9,7 +9,7 @@ import SubBg from "../../../assets/img/sub-bg.png"
 function Sub() {
 
   return (
-    <section className="relative my-[50px]">
+    <section className="relative my-[50px] py-[120px]">
       <h2 className="text-center absolute top-[350px] mx-10  z-[10]  text-[64px] font-bold custom-text-shadow ">一人旅をしよう</h2>
       <div className="relative place-items-center">
         <SubSwipe />
