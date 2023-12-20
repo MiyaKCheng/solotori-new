d03c8332fde8c670d1f7cef18c0d1aefe91311a0
MiyaKCheng <miya1736@gmail.com>
2023-12-20 16:30:41 +0900

    modified:   src/pages/home/kv/Kv.jsx

--- src/pages/home/kv/Kv.jsx
+++ src/pages/home/kv/Kv.jsx
@@ -14,7 +14,7 @@ function Kv() {
           <img
             src={kv01}
             alt="Key Visual Image"
-            className="relative block border-[3px] border-white kvShadow rounded-[15px]  -rotate-12 w-60 sm:top-[75px] left-[55px] inset-0 transform-gpu  backdrop-blur-3xl blur-img bg-[rgba(225,225,225,0.3)]" />
+            className="relative block border-[3px] border-white kvShadow rounded-[15px]  -rotate-12 min-[320px]:w-[220px] w-60 min-[320px]:top-[75px] sm:top-[85px] left-[55px] inset-0 transform-gpu  backdrop-blur-3xl blur-img bg-[rgba(225,225,225,0.3)]" />
           <div className="absolute top-0 right-0 mt-[-80px] mr-2 z-[-10] w-20">
             <NewNormal />
           </div>
@@ -26,28 +26,28 @@ function Kv() {
         </div>
 
 
-        <div className="absolute bottom-[-10px] pb-[130px] ml-3 h-200">
-          <div className="grid grid-cols-10">
-            <p className="text-[20px] text-center tracking-widest bg-main-color bg-cover col-start-1 col-end-8 row-span-1 ">
+        <div className="absolute min-[389px]:bottom-[-10px] bottom-[-70px] min-[320px]:pb-[160px] ml-3 h-200">
+          <div className="box-border w-[100%]">
+            <p className="float-left  text-[20px] w-[68%] text-center tracking-widest bg-main-color bg-cover ">
               クリエイター
             </p>
-            <p className="mb-[-15px] mt-[-10px] text-[90px] lg:mt-8 custom-text-shadow col-start-1 col-end-10">
+            <p className=" float-left w-[100%] mb-[-15px] mt-[-10px]  ml-[-4px] text-[5.6rem] custom-text-shadow">
               募集中
             </p>
-            <div className="text-main-color mb-[-5px] mr-[-5px] text-[10px] flex justify-between items-center col-start-1 col-end-8 row-span-1 tracking-[4px]">
-              <p className="col-start-1 col-end-4">2024年</p>
-              <p className="col-start-5 col-end-8" >SOLOTORI公式</p>
+            <div className="w-[69%] text-main-color  text-[10px] flex justify-between items-center tracking-[4px]">
+              <p className="float-left">2024年</p>
+              <p className="float-right" >SOLOTORI公式</p>
             </div>
-            <div className=" flex col-start-1 col-end-8 items-center">
-              <p className="font-light text-[8px] bg-main-color bg-cover pl-1.5 p-1 rounded border border-white tracking-[.25em] text-white">
+            <div className=" w-[100%] flex items-center  mt-[4px] ">
+              <p className="float-left w-[35px] h-[35px] font-light text-[8px] bg-main-color bg-cover pl-2 p-1 rounded border border-white tracking-[.25em] text-white">
                 応募<br />期間
               </p>
-              <div className="text-main-color text-[28px] pl-2">
+              <div className="float-right text-main-color text-[2rem] pl-1">
                 1.01
-                <span className="text-xs ml-1">MON</span>
-                <span className="underline decoration-2 text-2xl ml-1 mr-1">　</span>
+                <span className="text-xs ml-[1px]">MON</span>
+                <span className="underline decoration-2 text-[15px] ml-1 mr-1">　</span>
                 2.29
-                <span className="text-xs ml-1">THU</span>
+                <span className="text-xs ml-[2px]">THU</span>
               </div>
             </div>
           </div>
