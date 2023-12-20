ea57b30411e5b8190b72bba207749650fa674e66
MiyaKCheng <miya1736@gmail.com>
2023-12-20 19:31:18 +0900

    modified:   package-lock.json 	modified:   package.json 	modified:   src/pages/home/info/Info.jsx 	modified:   src/pages/home/info/info.css 	modified:   tailwind.config.js 	modified:   src/pages/home/info/info.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,30 +6,32 @@ export default {
   ],
   theme: {
     extend: {
-      backgroundColor: ['active'],
-      colors: {
-        'main-color': '#F0967d',
-        'ac-color': '#D13200',
-      },
-      animation: {
-        'spin-slow': 'spin 30s linear infinite',
-      },
-      textFillColor: theme => theme('borderColor'),
-      textStrokeColor: theme => theme('borderColor'),
-      textStrokeWidth: theme => theme('borderWidth'),
-      paintOrder: {
-        'fsm': { paintOrder: 'fill stroke markers' },
-        'fms': { paintOrder: 'fill markers stroke' },
-        'sfm': { paintOrder: 'stroke fill markers' },
-        'smf': { paintOrder: 'stroke markers fill' },
-        'mfs': { paintOrder: 'markers fill stroke' },
-        'msf': { paintOrder: 'markers stroke fill' },
-      },
+    backgroundColor: ['active'],
+    colors: {
+      'main-color': '#F0967d',
+      'ac-color': '#D13200',
+    },
+    animation: {
+      'spin-slow': 'spin 30s linear infinite',
+    },
+    textFillColor: theme => theme('borderColor'),
+    textStrokeColor: theme => theme('borderColor'),
+    textStrokeWidth: theme => theme('borderWidth'),
+    paintOrder: {
+      'fsm': { paintOrder: 'fill stroke markers' },
+      'fms': { paintOrder: 'fill markers stroke' },
+      'sfm': { paintOrder: 'stroke fill markers' },
+      'smf': { paintOrder: 'stroke markers fill' },
+      'mfs': { paintOrder: 'markers fill stroke' },
+      'msf': { paintOrder: 'markers stroke fill' },
     },
 
   },
-  plugins: [
-    require('tailwindcss-text-fill-stroke'),
-    require('@tailwindcss/forms'),
-  ],
+
+},
+plugins: [
+  require('tailwindcss-text-fill-stroke'),
+  require('@tailwindcss/forms'),
+  require('@tailwindcss/typography'),
+],
 }
