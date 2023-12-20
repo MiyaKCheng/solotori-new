ea6c85e109bc14d61109c2d5e6b5c676d2dd30b7
MiyaKCheng <miya1736@gmail.com>
2023-12-20 14:41:58 +0900

    modified:   src/pages/home/top/Top.jsx

--- src/pages/home/top/Top.jsx
+++ src/pages/home/top/Top.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Menu from "../menu/Menu.jsx"; // Import Hamburger component
-
+import logo from "../../../assets/logo/logo.png"
 function Top() {
   return (
     <>
       <div >
         <div id="top" className="flex justify-between mr-3 ml-4 my-5">
-          <a href="/"><img src="../../../assets/logo/logo.png" alt="SOLOTORI スペシャルサイトlogo" /></a>
+          <a href="/"><img src={logo} width="146px" alt="SOLOTORI スペシャルサイトlogo" /></a>
           <Menu />
         </div>
       </div>
