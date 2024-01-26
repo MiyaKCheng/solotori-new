8a59c7312c477bccb2e98d83626ad137a60db4c2
MiyaKCheng <miya1736@gmail.com>
2024-01-26 09:19:05 +0900

    modified:   src/pages/home/sub/Sub.jsx

--- src/pages/home/sub/Sub.jsx
+++ src/pages/home/sub/Sub.jsx
@@ -10,7 +10,7 @@ function Sub() {
 
   return (
     <section className="relative my-[50px] py-[120px]">
-      <h2 className="text-center absolute top-[350px] mx-[50px] mt-32  z-[10]  text-[64px] font-bold custom-text-shadow ">一人旅をしよう</h2>
+      <h2 className="text-center absolute top-[350px] ml-[52px] mr-[52px] mt-32  z-[10]  text-[64px] font-bold custom-text-shadow ">一人旅をしよう</h2>
       <div className="relative place-items-center">
         <SubSwipe />
         <Spin />
@@ -20,7 +20,7 @@ function Sub() {
         alt="Under Water画像"
         className="sub-bg absolute top-[0px] z-[-99] w-100% h-[950px] mix-blend-soft-light opacity-30 "
       />
-    </section>
+    </section >
   );
 }
 
